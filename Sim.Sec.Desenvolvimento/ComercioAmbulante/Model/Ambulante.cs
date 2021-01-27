9ba4d182406d7454f7ff79255e3f79f7fd69c296
Fernando Almeida <fernando.ralmeida@outlook.com>
2021-01-27 17:07:28 -0300

    up_27_01_2021

--- Sim.Sec.Desenvolvimento/ComercioAmbulante/Model/Ambulante.cs
+++ Sim.Sec.Desenvolvimento/ComercioAmbulante/Model/Ambulante.cs
@@ -6,11 +6,80 @@ using System.Threading.Tasks;
 
 namespace Sim.Sec.Desenvolvimento.ComercioAmbulante.Model
 {
-    public class Ambulante : DIA
+    using Mvvm.Observers;
+    public class Ambulante : NotifyProperty
     {
-        private bool _temempresa;
-        public bool TemCNPJ { get { return _temempresa; } set { _temempresa = value; RaisePropertyChanged("TemCNPJ"); } }
-  
+        #region Declarations
+        private int _indice = 0;
+        private string _cadastro = string.Empty;
+        private Autorizados _titular = new Autorizados();
+        private Autorizados _auxiliar = new Autorizados();
+        private string _formaatuacao = string.Empty;
+        private string _periodotrabalho = string.Empty;
+        private string _local = string.Empty;
+        private string _atividade = string.Empty;
+        private DateTime _datacadastro = DateTime.Now;
+        private DateTime _dataalteracao = DateTime.Now;
+        private bool _ativo = true;
+        #endregion
 
+        #region Properties
+        public int Indice { get { return _indice; } set { _indice = value; RaisePropertyChanged("Indice"); } }
+        public string Cadastro
+        {
+            get { return _cadastro; }
+            set { _cadastro = value; RaisePropertyChanged("Cadastro"); }
+        }
+        public Autorizados Titular
+        {
+            get { return _titular; }
+            set { _titular = value; RaisePropertyChanged("Titular"); }
+        }
+        public Autorizados Auxiliar
+        {
+            get { return _auxiliar; }
+            set { _auxiliar = value; RaisePropertyChanged("Auxiliar"); }
+        }
+        public string FormaAtuacao
+        {
+            get { return _formaatuacao; }
+            set { _formaatuacao = value; RaisePropertyChanged("TipoInstalacoes"); }
+        }
+        public string PeridoTrabalho
+        {
+            get { return _periodotrabalho; }
+            set { _periodotrabalho = value; RaisePropertyChanged("PeriodoTrabalho"); }
+        }
+        public string Local
+        {
+            get { return _local; }
+            set { _local = value; RaisePropertyChanged("Local"); }
+        }
+        public string Atividade
+        {
+            get { return _atividade; }
+            set { _atividade = value; RaisePropertyChanged("Atividade"); }
+        }
+
+        public DateTime DataCadastro
+        {
+            get { return _datacadastro; }
+            set { _datacadastro = value; RaisePropertyChanged("DataCadastro"); }
+        }
+        public DateTime DataAlteracao
+        {
+            get { return _dataalteracao; }
+            set { _dataalteracao = value; RaisePropertyChanged("DataAlteracao"); }
+        }
+        public bool Ativo
+        {
+            get { return _ativo; }
+            set
+            {
+                _ativo = value; RaisePropertyChanged("Ativo");
+            }
+        }
+        public int Contador { get; set; }
+        #endregion
     }
 }
